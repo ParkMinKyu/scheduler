da310ef269d67e299d673854c4fef5653bbea2e5
niee <niee@naver.com>
2017-02-12 00:54:05 +0900

    comment add

--- dao/schedulerdao.py
+++ dao/schedulerdao.py
@@ -82,11 +82,15 @@ def putScheduler(schedule):
 
 # parameter 빈값 확인
 def parameter_checker(params):
+    #parameter type check
     typeName = type(params)
+    #0이거나 공백일경우 false
     if not bool(params):
         return False
+    #str 이나 unicode일경우 trim 된 문자열이 공백인지 확인
     elif typeName == str or typeName == unicode and not bool(params.strip()):
         return False
+    # Dictoionary나 ImmutableMultiDict형일경우 변수 순환하며 체크
     elif typeName == dict or typeName == ImmutableMultiDict:
         for key in params.keys() :
             if not parameter_checker(params[key]) :
